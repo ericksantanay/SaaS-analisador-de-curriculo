28a8f3b801cf3d5ddeb7c0febd9f266827b8179f
Erick Silva <donizttierick@gmail.com>
2026-08-19 17:12:44 -0300

    .

--- backend/src/services/emailService.ts
+++ backend/src/services/emailService.ts
@@ -1,7 +1,7 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 
 // Create a transporter using SMTP
-
-
+const transporter = nodemailer.
+// Ajustar o metodo. O vscode nao esta recomendando. AJustar isso tambem!
 // smtp.gmail.com
 // 587
